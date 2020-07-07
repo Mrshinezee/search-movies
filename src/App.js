2becf7c63188d334c57f129f59247d1124bd6093
Mrshinezee <ijeomaharinze@gmail.com>
2020-07-07 10:12:03 +0100

    Styling the search result

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import store from './store';
 import Search from './containers/Search/Search'
 
--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,38 +1,45 @@
-import React from 'react'
+import React, { useState} from 'react'
 import PropTypes from 'prop-types'
+import ReactHtmlParser from 'react-html-parser';
 
 import Loader from '../Loader'
 
 
 import './SearchResults.css'
 
-const MovieCard = ({ episode }) => {
-    const { image } = episode;
-    return (
-     <a href={episode.url}>
-       <div className="card">
-      { image ? <img
-          className="card-img-top"
-          src={image ? image.medium : ''}
-          alt="Episode Card preview"
-        />
-        : <div className="card-no-img"> No preview image</div>
-      }
-        <div className="card-body">
-          <h5 className="card-title">{episode.name} ({episode.airdate.split('-')[0]})</h5>
-          <h6 className="card-subtitle mb-2 text-muted">Season {episode.season} Episode {episode.number}</h6>
-          {/* {episode.summary && ReactHtmlParser(episode.summary)} */}
-        </div>
-      </div>
-     </a>
-    );
-  }
-
 
 const SearchResults = ({isLoading, error, data }) => {
-//   console.log('display', isLoading, error, data)
+  const [seasonSelect, setSeason] = useState({ sess: 1,});
+  let filteredSeason = [];
   let { episodes } = data._embedded;
+  const seasons = []
 
+  episodes.forEach((episode) => {
+    const { season } = episode
+
+    if(seasons[season]) {
+        seasons[season].push(episode)
+    } else {
+        seasons[season] = [episode]
+    }
+  })
+
+  console.log(Object.keys(seasons))
+
+  const handleSelectSeason = (evt) => {
+    const value = evt.target.value;
+    setSeason({
+        ...seasonSelect,
+        sess: value
+    });
+}
+
+
+//   let unique = [...new Set(episodes.map(item => item.season))];
+//   unique.forEach(season => {
+//       let selectedSeason = episodes.filter(epi => epi.season === season);
+//       filteredSeason.push(filteredSeason[`season ${season}`] = selectedSeason);
+// });
   let movieDetailsList = []
 
   if (isLoading) {
@@ -44,32 +51,69 @@ const SearchResults = ({isLoading, error, data }) => {
   }
 
   if (!isLoading && !error && data) {
-      movieDetailsList = episodes.map(episode => (
-        <MovieCard key={episode.id} episode={episode} />
-      ))
+    //   movieDetailsList = episodes.map(episode => (
+    //     <MovieCard key={episode.id} episode={episode} />
+    //   ))
   }
 
-  if (data && data.length === 0) {
+  if (error) {
     return (
       <div className='search-results__none'>
         No Results Found
       </div>
     )
   }
- console.log('data data', data)
+
   return (
     <div className="search__container">
-        <div className="search__result__container">
-            <h1>{data.name}</h1>
-            <div>
-            { data.image ? <img
-            className="title-card-img-top"
-            src={data && data.image ? data.image.medium : ''}
-            alt="search preview"
-            />
-            : <div className="card-no-img"> No preview image</div>
-            }
+        <div className="search__result__container container">
+            <div className="row">
+                <div className="col-sm-4 col-xs-12 text-center">
+                    { data.image ? <img
+                    className="tit-card-img-top"
+                    src={data && data.image ? data.image.medium : ''}
+                    alt="search preview"
+                    />
+                    : <div className="card-no-img"> No preview image</div>
+                    }   
+                </div>
+                <div className="col-sm-8 col-xs-12">
+                    <h3>{data.name}</h3>
+                    <h5 className="text-success">Description:</h5>
+                    <h6>{data.summary && ReactHtmlParser(data.summary)}</h6>
+                    <h6><span className="text-success">Country: </span>{data.language}</h6>
+                    <h6><span className="text-success">Status: </span>{data.status}</h6>
+                    <h6><span className="text-success">Genres: </span>{data.genres.join(', ')}</h6>
+                    <div className="m-1"><span className="text-success">SEASON: </span>
+                        <select value={seasonSelect.sess} onChange={handleSelectSeason}>
+                                    {Object.keys(seasons).map((sea, index) => <option key={index} value={sea}>{`season ${sea}`}</option>)}
+                                </select>
+                    </div>
+                </div>
             </div>
+
+<div className="container">
+<h3 className="text-success"> Season {seasonSelect.sess}</h3>
+<table className="table m-2 overflow-auto">
+  <thead className="bg-success">
+    <tr>
+      <th scope="col">Episode</th>
+      <th scope="col">Name</th>
+      <th scope="col">Link</th>
+    </tr>
+  </thead>
+  <tbody>
+  {seasons[seasonSelect.sess].map((episode, index) =>
+    <tr key={index}>
+      <th scope="row">{episode.number}</th>
+      <td>{episode.name}</td>
+      <td><a className="text-success" href={episode.url}> {episode.url} </a></td>
+    </tr>
+    )}
+  </tbody>
+</table>
+</div>
+
             <div className='search-results'>
             {movieDetailsList}
             </div>
